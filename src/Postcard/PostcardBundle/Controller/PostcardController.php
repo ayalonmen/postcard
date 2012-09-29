88e35c89e48418ae4ca26c7aa28033aeae0b3ee8
jonathan bensaid <john@bensaidj.com>
2012-09-29 18:05:43 +0200

    Implement index Postcard

--- src/Postcard/PostcardBundle/Controller/PostcardController.php
+++ src/Postcard/PostcardBundle/Controller/PostcardController.php
@@ -8,6 +8,11 @@ class PostcardController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('PostcardPostcardBundle:Postcard:index.html.twig');
+    	$em = $this->getDoctrine()->getEntityManager();
+    	$postcards = $em->getRepository('PostcardPostcardBundle:Postcard')->findAll();
+
+        return $this->render('PostcardPostcardBundle:Postcard:index.html.twig',array(
+        	'postcards'=>$postcards,
+        ));
     }
 }
