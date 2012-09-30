c576b00867211f080e79ac840b3cddcb4ce1338f
jonathan bensaid <john@bensaidj.com>
2012-09-30 07:00:14 +0200

    Update phpdoc

--- src/Postcard/PostcardBundle/Controller/PostcardController.php
+++ src/Postcard/PostcardBundle/Controller/PostcardController.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of Postcard application
+ *
+ * (c) Jonathan Bensaid <john@bensaidj.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Postcard\PostcardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -7,8 +16,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Postcard\PostcardBundle\Form\Type\PostcardType;
 use Postcard\PostcardBundle\Entity\Postcard;
 
+/**
+ * Controller managing Postcard CRUD
+ *
+ * @author Jonathan Bensaid <john@bensaidj.com>
+ */
 class PostcardController extends Controller
 {
+    /**
+     * Retrieve all the Postcards
+     *
+     * Retrieve the Postcards and present them from the oldest to the newest
+     */
     public function indexAction()
     {
     	$em = $this->getDoctrine()->getEntityManager();
@@ -21,6 +40,13 @@ class PostcardController extends Controller
         ));
     }
 
+    /**
+     * Display one Postcard
+     *
+     * Display the Postcard identified by its id
+     *
+     * @param integer $id    The id of the requested Postcard
+     */
     public function showAction($id)
     {
     	$em = $this->getDoctrine()->getEntityManager();
@@ -33,6 +59,13 @@ class PostcardController extends Controller
     	));
     }
 
+    /**
+     * Create a new Postcard
+     *
+     * Display the form associated with a new Postcard with a GET Request or treat the persistence of the Postcard with a POST Reques
+     *
+     * @param Request $request    The request object sent by the client
+     */
     public function newAction(Request $request)
     {
         $postcard = new Postcard();
--- src/Postcard/PostcardBundle/Model/PostcardInterface.php
+++ src/Postcard/PostcardBundle/Model/PostcardInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of Postcard application
+ *
+ * (c) Jonathan Bensaid <john@bensaidj.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Postcard\PostcardBundle\Model;
 
 interface PostcardInterface
