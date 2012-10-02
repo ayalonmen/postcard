69005983a001e1c569edf3c07d59bc0254735d0e
jonathan bensaid <john@bensaidj.com>
2012-10-02 04:41:46 +0200

    Update capifony settings

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,14 +1,21 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
+set :application, "postcard"
+set :domain,      "#{application}.john-benz.com"
 set :deploy_to,   "/var/www/#{domain}"
 set :app_path,    "app"
+set :web_path,    "web"
+set :user,        "john"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
+# Git Repository
+set :repository,  "git@github.com:johnbenz13/postcard.git"
 set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+set :branch, "master"
+ssh_options[:forward_agent] = true
 
+# Database
 set :model_manager, "doctrine"
-# Or: `propel`
+
+# Vendors
+set :use_composer, true
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
