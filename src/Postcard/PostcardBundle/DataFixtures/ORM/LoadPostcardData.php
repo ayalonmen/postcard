453f1c109fd5dfd63135848576ca357ab3c790a1
jonathan bensaid <john@bensaidj.com>
2012-10-19 11:56:11 +0200

    Update Postcard fixtures with PostcardManager

--- src/Postcard/PostcardBundle/DataFixtures/ORM/LoadPostcardData.php
+++ src/Postcard/PostcardBundle/DataFixtures/ORM/LoadPostcardData.php
@@ -3,54 +3,59 @@ namespace Postcard\PostcardBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Postcard\PostcardBundle\Entity\Postcard;
 
-class LoadPostcardData extends AbstractFixture implements OrderedFixtureInterface
+class LoadPostcardData extends AbstractFixture implements ContainerAwareInterface, OrderedFixtureInterface
 {
+	private $container;
+
+	private $postcardManager;
+
+	public function setContainer(ContainerInterface $container = null)
+	{
+		$this->container = $container;
+		$this->postcardManager = $container->get('postcard_postcard.postcard_manager');
+	}
+
 	public function load(ObjectManager $manager)
 	{
-		$postcard1 = new Postcard();
+		$postcard1 = $this->postcardManager->createPostcard($manager->merge($this->getReference('john')));
 		$postcard1->setTitle("Hello from TLV")
 				  ->setLocation("Tel Aviv, Israel")
 				  ->setBody("Hi everybody! I just wanted you to see how beautiful it is here in Tel Aviv. I having a lot of fun! Miss You, Bobby!")
-				  ->setPicture("telaviv.jpg")
-				  ->setSender($manager->merge($this->getReference('john')));
-		$manager->persist($postcard1);
+				  ->setPicture("telaviv.jpg");
+		$this->postcardManager->updatePostcard($postcard1, false);
 
-		$postcard2 = new Postcard();
+		$postcard2 = $this->postcardManager->createPostcard($manager->merge($this->getReference('tony')));
 		$postcard2->setTitle("Hello from New York")
 				  ->setLocation("New York City, USA")
 				  ->setBody("Hi everybody! I just wanted you to see how beautiful it is here in NEW York. I having a lot of fun! Miss You, Bobby!")
-				  ->setPicture("nyc.jpg")
-				  ->setSender($manager->merge($this->getReference('tony')));
-		$manager->persist($postcard2);
+				  ->setPicture("nyc.jpg");
+		$this->postcardManager->updatePostcard($postcard2, false);
 
-		$postcard3 = new Postcard();
+		$postcard3 = $this->postcardManager->createPostcard($manager->merge($this->getReference('tessa')));
 		$postcard3->setTitle("Hello from Paris")
 				  ->setLocation("Paris, France")
 				  ->setBody("Hi everybody! I just wanted you to see how beautiful it is here in Paris. I having a lot of fun! Miss You, Bobby!")
-				  ->setPicture("paris.jpg")
-				  ->setSender($manager->merge($this->getReference('tessa')));
-		$manager->persist($postcard3);
+				  ->setPicture("paris.jpg");
+		$this->postcardManager->updatePostcard($postcard3, false);
 
-		$postcard4 = new Postcard();
+		$postcard4 = $this->postcardManager->createPostcard($manager->merge($this->getReference('john')));
 		$postcard4->setTitle("Hello from London")
 				  ->setLocation("London, England")
 				  ->setBody("Hi everybody! I just wanted you to see how beautiful it is here in London. I having a lot of fun! Miss You, Bobby!")
-				  ->setPicture("london.jpg")
-				  ->setSender($manager->merge($this->getReference('john')));
-		$manager->persist($postcard4);
+				  ->setPicture("london.jpg");
+		$this->postcardManager->updatePostcard($postcard4, false);
 
-		$postcard5 = new Postcard();
+		$postcard5 = $this->postcardManager->createPostcard($manager->merge($this->getReference('tony')));
 		$postcard5->setTitle("Hello from Berlin")
 				  ->setLocation("Berlin, Germany")
 				  ->setBody("Hi everybody! I just wanted you to see how beautiful it is here in Berlin. I having a lot of fun! Miss You, Bobby!")
-				  ->setPicture("berlin.jpg")
-				  ->setSender($manager->merge($this->getReference('tony')));
-		$manager->persist($postcard5);
-
-		$manager->flush();
+				  ->setPicture("berlin.jpg");
+		$this->postcardManager->updatePostcard($postcard5);
 	}
 
 	public function getOrder()
